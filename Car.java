c7208fa4b532dcd126cbee0eaac8c948412ecce5
Daniyal Mushtaq <daniyalawan0101@gmail.com>
2026-08-04 08:36:28 +0500

    Add files via upload

---
+++ Car.java
@@ -0,0 +1,72 @@
+public class Car {
+    // Public fields
+    public String brand;
+    public String model;
+    public int year;
+    public double speed;
+
+    // Constructor
+    public Car(String brand, String model, int year) {
+        this.brand = brand;
+        this.model = model;
+        this.year = year;
+        this.speed = 0.0;
+    }
+
+    // Getters
+    public String getBrand() {
+        return brand;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public double getSpeed() {
+        return speed;
+    }
+
+    // Behavior methods
+    public void accelerate(double amount) {
+        if (amount > 0) {
+            speed += amount;
+            System.out.println(brand + " " + model + " accelerated to " + speed + " km/h");
+        }
+    }
+
+    public void brake(double amount) {
+        speed -= amount;
+        if (speed < 0) {
+            speed = 0;
+        }
+        System.out.println(brand + " " + model + " slowed down to " + speed + " km/h");
+    }
+
+    public void displayInfo() {
+        System.out.println("Car: " + year + " " + brand + " " + model);
+        System.out.println("Current Speed: " + speed + " km/h");
+    }
+
+    // Main method
+    public static void main(String[] args) {
+        Car car1 = new Car("Toyota", "Corolla", 2022);
+        Car car2 = new Car("Honda", "Civic", 2023);
+
+        car1.displayInfo();
+        car1.accelerate(40);
+        car1.accelerate(20);
+        car1.brake(15);
+        car1.displayInfo();
+
+        System.out.println();
+
+        car2.displayInfo();
+        car2.accelerate(60);
+        car2.brake(70);
+        car2.displayInfo();
+    }
+}
